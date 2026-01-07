4c95e98880e09a46662077593ebb3e3023f86d4e
Prath4m-cmd <prathameshnandwadekar@gmail.com>
2026-01-07 21:49:54 +0530

    change in Data Transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -31,7 +31,7 @@ class DataTransformation:
 
             num_pipeline = Pipeline(steps=[
                 ('imputer', SimpleImputer(strategy='median')),
-                ('scaler', StandardScaler(with_mean=False))
+                ('scaler', StandardScaler())
             ])
 
             cat_pipeline = Pipeline(steps=[
